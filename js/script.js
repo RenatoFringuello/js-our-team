48392477fbe4de91a771537db29f0fc029deccb6
RenatoFringuello <renatofringuello@hotmail.it>
2022-12-05 13:30:11 +0100

    create list team

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const team = [
+    {
+        firstName: 'Wayne', 
+        surname : 'Barnett',
+        role : 'Founder & CEO',
+        imgName : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        firstName: 'Angela', 
+        surname : 'Caroll',
+        role : 'Chief Editor',
+        imgName : 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        firstName: 'Walter', 
+        surname : 'Gordon',
+        role : 'Office Manager',
+        imgName : 'walter-gordon-office-manager.jpg'
+    },
+    {
+        firstName: 'Angela', 
+        surname : 'Lopez',
+        role : 'Social Media Manager',
+        imgName : 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        firstName: 'Scott', 
+        surname : 'Estrada',
+        role : 'Developer',
+        imgName : 'scott-estrada-developer.jpg'
+    },
+    {
+        firstName: 'Barbara', 
+        surname : 'Ramos',
+        role : 'Graphic Designer',
+        imgName : 'barbara-ramos-graphic-designer.jpg'
+    }
+];
